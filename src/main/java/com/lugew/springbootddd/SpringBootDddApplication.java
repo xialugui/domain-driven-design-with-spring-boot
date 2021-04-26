60b9879c8cccedbd7d5716a734a1638bb01eacec
xialugui <xialugui@mimovr.com>
2021-04-26 18:06:00 +0800

    feature(init): init

---
+++ src/main/java/com/lugew/springbootddd/SpringBootDddApplication.java
@@ -0,0 +1,13 @@
+package com.lugew.springbootddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDddApplication.class, args);
+    }
+
+}
