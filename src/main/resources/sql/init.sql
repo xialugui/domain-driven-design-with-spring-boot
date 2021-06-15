a5ff597930e1b089364456b41098d59b58fad3c8
xialugui <xialugui@mimovr.com>
2021-06-15 11:10:31 +0800

    feature(JPA): add orm

---
+++ src/main/resources/sql/init.sql
@@ -0,0 +1,4 @@
+insert into snack_machine_dto (five_dollar_count, money_in_transaction,
+                               one_cent_count, one_dollar_count,
+                               quarter_count, ten_cent_count, twenty_dollar_count, id)
+values (1, 0.0, 1, 1, 1, 1, 1, 1);
